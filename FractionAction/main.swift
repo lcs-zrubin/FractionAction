5d4716b8dd918cbf3950f1da7057a202bde4b278
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-04-26 08:21:46 -0400

    Initial Commit

---
+++ FractionAction/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  ChipsFastFoodEmporium
+//
+//  Created by Gordon, Russell on 2018-04-04.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+// INPUT
+// Collect and filter user input here
+
+
+// PROCESS
+// Implement the primary logic of the problem here
+
+
+// OUTPUT
+// Report results to the user here
+
